f913aafc6ab0fb1fe54e47d392df596e49868893
JIASHI0330 <jiaswang@student.unimelb.edu.au>
2024-09-15 03:17:49 +1000

    add more questions

--- String/Leetcode844TypedOutString.py
+++ String/Leetcode844TypedOutString.py
@@ -8,43 +8,43 @@ Questions to ask during the interview to clarify the constraints
 
 
 # Brute force solution
-# def backspaceCompare(s, t):
-#     new_s = []
-#     new_t = []
-#     for i in range(len(s)):
-#         if s[i] == "#":
-#             if len(new_s) != 0:
-#                 new_s.pop()
-#         else:
-#             new_s.append(s[i])
-#
-#     for j in range(len(t)):
-#         if t[j] == "#":
-#             if len(new_t) != 0:
-#                 new_t.pop()
-#         else:
-#             new_t.append(t[j])
-#
-#     if len(new_s) != len(new_t):
-#         return False
-#
-#     if new_s == new_t:
-#         return True
-#     else:
-#         return False
-
 def backspaceCompare(s, t):
-    sPointer = len(s) - 1
-    tPointer = len(t) - 1
-    while (sPointer >= 0) or (tPointer >= 0):
-        if s[sPointer] == "#" or t[tPointer] == "#":
-            if s[sPointer] == "#":
-                jumpCount = 2
-                while jumpCount > 0:
-                    sPointer -= 1
-                    jumpCount -= 1
-                    if s[sPointer] == "#":
-                        jumpCount = jumpCount + 2
+    new_s = []
+    new_t = []
+    for i in range(len(s)):
+        if s[i] == "#":
+            if len(new_s) != 0:
+                new_s.pop()
+        else:
+            new_s.append(s[i])
+
+    for j in range(len(t)):
+        if t[j] == "#":
+            if len(new_t) != 0:
+                new_t.pop()
+        else:
+            new_t.append(t[j])
+
+    if len(new_s) != len(new_t):
+        return False
+
+    if new_s == new_t:
+        return True
+    else:
+        return False
+
+# def backspaceCompare(s, t):
+#     sPointer = len(s) - 1
+#     tPointer = len(t) - 1
+#     while (sPointer >= 0) or (tPointer >= 0):
+#         if s[sPointer] == "#" or t[tPointer] == "#":
+#             if s[sPointer] == "#":
+#                 jumpCount = 2
+#                 while jumpCount > 0:
+#                     sPointer -= 1
+#                     jumpCount -= 1
+#                     if s[sPointer] == "#":
+#                         jumpCount = jumpCount + 2
 
 
 
